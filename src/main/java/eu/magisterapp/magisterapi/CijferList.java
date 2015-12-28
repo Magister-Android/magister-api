257a8f3dec8212ded20b481231f830f22f7f7fe3
Max Verbeek <m4xv3rb33k@gmail.com>
2015-12-28 21:15:50 +0100

    Misschien is dit gesorteerd op vak, moet nog testen. lol kutajva

--- src/main/java/eu/magisterapp/magisterapi/CijferList.java
+++ src/main/java/eu/magisterapp/magisterapi/CijferList.java
@@ -5,12 +5,16 @@ import org.json.JSONException;
 
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by max on 2-12-15.
  */
 public class CijferList extends ArrayList<Cijfer>
 {
+    public final Map<Integer, CijferList> vakSorted = new HashMap<>();
+
     public static CijferList fromResponse(Response response, VakList vakken) throws ParseException, JSONException
     {
         return new CijferList(response.getJson().getJSONArray("Items"), vakken);
@@ -26,6 +30,13 @@ public class CijferList extends ArrayList<Cijfer>
             if (cijfer.CijferStr == null) continue; // skip bullshitcijfers
 
             this.add(cijfer);
+
+            if (cijfer.Vak == null || cijfer.Vak.Id == null) continue;
+
+            if (vakSorted.get(cijfer.Vak.Id) == null)
+                vakSorted.put(cijfer.Vak.Id, new CijferList());
+
+            vakSorted.get(cijfer.Vak.Id).add(cijfer);
         }
     }
 }
